1618cbeb1431579f08ff34c4b5485c91658af7d8
Thomas J Bradley <theman@thomasjbradley.ca>
2011-05-10 21:03:31 -0400

    Updating code formatting styles.

--- build.js
+++ build.js
@@ -10,23 +10,19 @@ var jshintrc = JSON.parse(fs.readFileSync('.jshintrc', 'utf8'))
 	,source = fs.readFileSync('src/jquery.signaturepad.js', 'utf8')
 	,valid = jshint(source, jshintrc)
 
-if(valid)
-{
+if (valid) {
 	process.stdout.write('done.\nMinifying... ')
 
 	exec('java -jar ~/bin/compiler.jar --js src/jquery.signaturepad.js --js_output_file assets/jquery.signaturepad.min.js'
-		,function(err, stdout, stderr){
+		,function(err, stdout, stderr) {
 			process.stdout.write('done.\n')
 	})
-}
-else
-{
+} else {
 	var e = null
 
 	console.log('\nJSHint failed with errors:')
 
-	for(var i=0, t=jshint.errors.length; i<t; i++)
-	{
+	for (var i = 0, t = jshint.errors.length; i < t; i++) {
 		e = jshint.errors[i]
 		console.log('• Line: ' + e.line + ' Col: ' + e.character + '; ' + e.reason)
 	}
